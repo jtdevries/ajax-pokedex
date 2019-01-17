2c20a83c8650f76665162afa705037f4f849f14a
Thijs <44265614+jtdevries@users.noreply.github.com>
2019-01-17 14:57:17 +0100

    updated css

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,23 +21,33 @@ $(document).ready(function(){
           $.ajax({url: pokeprevurl, success: function(z){
 
           console.log(pokeprevurl);
-
+          
           $('.imgFive').attr('src', z.sprites.front_default);
         }
       });
+        if (y.evolves_from_species == null) {
+          $('.six').text('No previous evolution ');
+        } else {
+          $('.six').text('Previous evolution: '+ y.evolves_from_species.name);
+        }
 
-        $('.six').text('Previous evolution: '+ y.evolves_from_species.name);
         $('.seven').text('Flavor text: '+ y.flavor_text_entries[1].flavor_text);
       }
     });
         $('h3').text('Name: '+ x.name);
-
         $('.one').text('ID: '+ x.id);
+        if (x.moves.length == 1) {
+          $('.two').text('Move 1: '+ x.moves[0].move.name);
+          $('.three').text('Move 2: '+ 'No move');
+          $('.four').text('Move 3: '+ 'No move');
+          $('.five').text('Move 4: '+ 'No move');
+        } else {
+          $('.two').text('Move 1: '+ x.moves[0].move.name);
+          $('.three').text('Move 2: '+ x.moves[1].move.name);
+          $('.four').text('Move 3: '+ x.moves[2].move.name);
+          $('.five').text('Move 4: '+ x.moves[3].move.name);
+        }
 
-        $('.two').text('Move 1: '+ x.moves[0].move.name);
-        $('.three').text('Move 2: '+ x.moves[1].move.name);
-        $('.four').text('Move 3: '+ x.moves[2].move.name);
-        $('.five').text('Move 4: '+ x.moves[3].move.name);
 
         $('.eight').text(x.height);
         $('.nine').text(x.weight);
