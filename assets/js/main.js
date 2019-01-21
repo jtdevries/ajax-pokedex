c085b827ae73e966493d0adf70b32071c04e82a2
Thijs <44265614+jtdevries@users.noreply.github.com>
2019-01-21 10:00:25 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,4 @@
-$(window).load(function() {
-  $(".imgOne").fadeOut()
-  $(".imgOne").delay(500)
-  $(".imgOne").fadeIn()
-});
+
 
 $(document).ready(function() {
   $('.search').click(function() {
@@ -23,7 +19,7 @@ $(document).ready(function() {
         $.ajax({
           url: url2,
           success: function(y) {
-            
+
             //CHECKS PREVIOUS EVOLUTION AND ADDS / REMOVES SPRITE
             if (y.evolves_from_species != null) {
               pokeprev = x.id - 1;
@@ -40,37 +36,37 @@ $(document).ready(function() {
 
             //CHECKS PREVIOUS EVOLUTION AND ADDS / REMOVES NAME
             if (y.evolves_from_species != null) {
-              $('.six').text('Previous evolution: ' + y.evolves_from_species.name);
+              $('.prevEvo').text('Previous evolution: ' + y.evolves_from_species.name);
             } else {
-              $('.six').text('No previous evolution ');
+              $('.prevEvo').text('No previous evolution ');
             }
 
             //RETURNS FLAVOUR TEXT
-            $('.seven').text('Flavor text: ' + y.flavor_text_entries[1].flavor_text);
+            $('.flavorText').text('Flavor text: ' + y.flavor_text_entries[1].flavor_text);
           }
         });
 
         //RETURNS NAME
         $('h3').text('Name: ' + x.name);
         //RETURNS ID
-        $('.one').text('ID: ' + x.id);
+        $('.id').text('ID: ' + x.id);
 
         //CHECKS IF POKEMON HAS ONLY 1 MOVE
         if (x.moves.length == 1) {
-          $('.two').text('Move 1: ' + x.moves[0].move.name);
-          $('.three').text('Move 2: ' + 'No move');
-          $('.four').text('Move 3: ' + 'No move');
-          $('.five').text('Move 4: ' + 'No move');
+          $('.move1').text('Move 1: ' + x.moves[0].move.name);
+          $('.move2').text('Move 2: ' + 'No move');
+          $('.move3').text('Move 3: ' + 'No move');
+          $('.move4').text('Move 4: ' + 'No move');
         } else {
-          $('.two').text('Move 1: ' + x.moves[0].move.name);
-          $('.three').text('Move 2: ' + x.moves[1].move.name);
-          $('.four').text('Move 3: ' + x.moves[2].move.name);
-          $('.five').text('Move 4: ' + x.moves[3].move.name);
+          $('.move1').text('Move 1: ' + x.moves[0].move.name);
+          $('.move2').text('Move 2: ' + x.moves[1].move.name);
+          $('.move3').text('Move 3: ' + x.moves[2].move.name);
+          $('.move4').text('Move 4: ' + x.moves[3].move.name);
         }
 
 
-        $('.eight').text(x.height);
-        $('.nine').text(x.weight);
+        $('.height').text(x.height);
+        $('.weight').text(x.weight);
 
         $('.imgOne').attr('src', x.sprites.front_default);
         $('.imgTwo').attr('src', x.sprites.back_default);
